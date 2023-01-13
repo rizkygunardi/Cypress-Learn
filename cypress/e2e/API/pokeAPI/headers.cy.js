941497a551054ef9fa8d322364af429a75683b8b
Rizky Gunardi <rgunardi97@gmail.com>
2023-01-13 16:18:42 +0700

    Tugas API Validate Content

--- cypress/e2e/API/pokeAPI/headers.cy.js
+++ cypress/e2e/API/pokeAPI/headers.cy.js
@@ -8,11 +8,42 @@ describe('Validate Header', () => {
 			.its('content-type')
 			.should('include', 'application/json; charset=utf-8')
 	})
+
+	//tugas Header
 	it('Successfully assert body', () => {
 		cy.request('GET', 'https://pokeapi.co/api/v2/pokemon/ditto').as('pika')
 		cy.get('@pika')
 			.its('body.abilities[0].ability.name')
 			.should('equal', 'limber')
 		cy.get('@pika').its('body.forms[0].name').should('equal', 'ditto')
+	})
+
+	it('Successfully validate with params', () => {
+		cy.request({
+			method: 'GET',
+			url: 'https://pokeapi.co/api/v2/pokemon/ditto',
+		}).as('salamander')
+		cy.get('@salamander').its('status').should('equal', 200)
+	})
+
+	it('Successfully validate with content', () => {
+		cy.request('https://pokeapi.co/api/v2/pokemon/bulbasaur').as(
+			'bulbasaur'
+		)
+		cy.get('@bulbasaur')
+			.its('body')
+			.should('include', { name: 'bulbasaur' })
+	})
+
+	//TUGAS VALIDATE CONTENT
+	it('Successfully assert body', () => {
+		cy.request('GET', 'https://pokeapi.co/api/v2/pokemon/ditto').then(
+			(response) => {
+				expect(response.body.abilities[0].ability.name).to.eq('limber')
+				expect(response.body.abilities[0].ability.url).to.eq(
+					'https://pokeapi.co/api/v2/ability/7/'
+				)
+			}
+		)
 	})
 })
